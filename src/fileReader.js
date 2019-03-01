1e7d946350e110e84ff8132e9623932328ea2c0b
Akar Khatab <Akar.Khatab@gmail.com>
2019-03-01 14:58:55 +0100

    Added method to read from files.

--- src/fileReader.js
+++ src/fileReader.js
@@ -1,33 +1,76 @@
 const fs = require('fs');
-const readline = require('readline');
 const chokidar = require('chokidar');
+// const Tail = require('tail').Tail;
+const lastLines = require('read-last-lines');
 
-const lineReader = require('readline').createInterface({
-  input: fs.createReadStream('myLittleFile.txt')
-});
+const readPos = fs.createReadStream('myLittleFile.txt');
 
-lineReader.on('line', line => {
-  console.log('Line:', line);
-});
+// readPos.on('open', () => {
+//   readPos.pipe(process.stdout);
+// });
 
-const read = readline.createInterface({
-  input: fs.createReadStream('myLittleFile.txt'),
-  output: process.stdout,
-  console: true
-});
+// const writeStream = fs.createWriteStream('myLittleFile.txt');
+// writeStream.once('open', fd => {
+//   for (let i = 0; i < 1000000; i++) {
+//     writeStream.write(`${i}\n`);
+//   }
+//   writeStream.end();
+// });
 
-read.on('line', line => {
-  console.log('line------:', line);
-});
+// let tailOptions = { follow: true };
+// const tail = new Tail('myLittleFile.txt');
 
-chokidar.watch('.', { ignored: /(^|[/\\])\../ }).on('all', (event, path) => {
-  console.log(event, path);
-});
+// tail.on('line', data => {
+//   console.log(data);
+// });
 
-fs.readFile('myLittleFile.txt', 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-});
+// tail.on('error', err => {
+//   console.log('ERROR: ', err);
+// });
+
+function readLastLines(filePath, numberOfLines) {
+  lastLines.read(filePath, numberOfLines).then(lines => {
+    console.log(lines);
+  });
+}
+
+function getNumberOfLines(filePath) {
+  return new Promise((resolve, reject) => {
+    let lineCount = 0;
+    fs.createReadStream(filePath)
+      .on('data', buffer => {
+        let idx = -1;
+        lineCount--;
+        do {
+          idx = buffer.indexOf(10, idx + 1);
+          lineCount++;
+        } while (idx !== -1);
+      })
+      .on('end', () => {
+        console.log(lineCount);
+        resolve(lineCount);
+      })
+      .on('error', reject);
+  });
+}
+
+function readFile(filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log(data);
+    console.log('DONE!');
+  });
+}
+
+function readFileWithCreateReadStream(filePath) {}
+
+// getNumberOfLines('myLittleFile.txt');
+// getNumberOfLines('text.txt');
+
+// readFile('myLittleFile.txt');
+// readFile('text.txt');
+
+readLastLines('myLittleFile.txt', 100);
+// readLastLines('text.txt', 10);
