9150f0916ac1669e393f8e7f61a8f32a6d366bbb
Emma Stålesjö <emma.stalesjo@kits.se>
2019-03-07 14:12:52 +0100

    #30 moved files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import React, { Component } from 'react';
-import logo from './log.jpg';
-import './App.css';
+const { React, Component } = require('react');
 
+const engine = require('./js/engine/engine');
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>We log everything!</p>
-          <a
-            className="App-link"
-            href="https://kits.se"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            kits.se
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
