dd5cc6fc4ee0a867a649ac6db53d9722c780e966
Joella <Joella@localhost.localdomain>
2019-03-08 09:19:06 +0100

    #24 added md5 module

--- src/watchFile.js
+++ src/watchFile.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const md5 = require('md5');
 
 const seeMeFile = './src/testFileForWatch.log';
 
 console.log(`Watching for file changes on ${seeMeFile}`);
 
+let md5Prevoius = null;
 let fsWait = false;
 
 fs.watch(seeMeFile, (event, filename) => {
@@ -14,6 +16,11 @@ fs.watch(seeMeFile, (event, filename) => {
     fsWait = setTimeout(() => {
       fsWait = false;
     }, 100);
+    const md5Current = md5(fs.readFileSync(seeMeFile));
+    if (md5Current === md5Prevoius) {
+      return;
+    }
+    md5Prevoius = md5Current;
     console.log(`${filename} file changed`);
   }
 });
