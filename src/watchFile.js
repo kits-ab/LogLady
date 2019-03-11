254e14a8a34b3c0786aa98928ca959023166dbe3
Joella <Joella@localhost.localdomain>
2019-03-11 11:21:32 +0100

    #24 use cokidar for watch

--- src/watchFile.js
+++ src/watchFile.js
@@ -1,26 +1,7 @@
-const fs = require('fs');
-const md5 = require('md5');
-
 const seeMeFile = './src/testFileForWatch.log';
 
-console.log(`Watching for file changes on ${seeMeFile}`);
+const chokidar = require('chokidar');
 
-let md5Prevoius = null;
-let fsWait = false;
-
-fs.watch(seeMeFile, (event, filename) => {
-  if (filename) {
-    if (fsWait) {
-      return;
-    }
-    fsWait = setTimeout(() => {
-      fsWait = false;
-    }, 100);
-    const md5Current = md5(fs.readFileSync(seeMeFile));
-    if (md5Current === md5Prevoius) {
-      return;
-    }
-    md5Prevoius = md5Current;
-    console.log(`${filename} file changed`);
-  }
+chokidar.watch(seeMeFile).on('all', (event, path) => {
+  console.log(event, path);
 });
