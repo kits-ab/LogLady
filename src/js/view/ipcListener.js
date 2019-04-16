3a01f7d6c4028fcf7e937b94b35e97faa5075fe3
Yoella <joella.j@hotmail.com>
2019-04-16 12:05:07 +0200

    #138 Filepath is redux

--- src/js/view/ipcListener.js
+++ src/js/view/ipcListener.js
@@ -1,8 +1,24 @@
+import { getLiveLines, getNumberOfLines, getFileSize } from './ipcPublisher';
 const { ipcRenderer } = window.require('electron');
 
-export const ipcListener = dispatch => {
+export const ipcListener = (dispatch, state) => {
   ipcRenderer.on('app_store', (event, action) => {
     switch (action.type) {
+      case 'menu_open':
+        dispatch({
+          type: action.type,
+          data: action.data
+        });
+        let argObj = {};
+        argObj.filePath = action.data[0];
+        argObj.numberOfLines = 5;
+        argObj.lineNumber = 10;
+
+        getLiveLines(argObj);
+        getNumberOfLines(argObj);
+        getFileSize(argObj);
+
+        break;
       default:
         dispatch({
           type: action.type,
