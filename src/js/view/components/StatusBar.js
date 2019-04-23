9a031b55d76bd2852eb1cd8899bc6618f694edbe
Akar Khatab <Akar.Khatab@gmail.com>
2019-04-23 16:24:49 +0200

    #167 Moved the function into a helper.

--- src/js/view/components/StatusBar.js
+++ src/js/view/components/StatusBar.js
@@ -2,23 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as StatusBarSC from '../styledComponents/StatusBarStyledComponents';
 import { handleShowSettings } from '../actions/dispatchActions';
+import { getFormattedFileSize } from './helpers/StatusBarHelper';
 const settings = require('../../../resources/settings.png');
 
 class StatusBar extends React.Component {
-  getFormattedFileSize = fileSize => {
-    if (fileSize >= 1000000000) {
-      return `${(fileSize / 1000000000).toFixed(2)} gigabytes`;
-    } else if (fileSize >= 1000000) {
-      return `${(fileSize / 1000000).toFixed(2)} megabytes`;
-    } else if (fileSize >= 1000) {
-      return `${(fileSize / 1000).toFixed(2)} kilobytes`;
-    } else if (fileSize) {
-      return `${fileSize} bytes`;
-    } else {
-      return `${0} bytes`;
-    }
-  };
-
   render() {
     return (
       <StatusBarSC.Statusbar>
@@ -32,7 +19,7 @@ class StatusBar extends React.Component {
         <ul>
           <li>Path: {this.props.openFiles ? this.props.openFiles[0] : null}</li>
           <li>Lines:{this.props.numberOfLines}</li>
-          <li>Size: {this.getFormattedFileSize(this.props.fileSize)}</li>
+          <li>Size: {getFormattedFileSize(this.props.fileSize)}</li>
         </ul>
       </StatusBarSC.Statusbar>
     );
