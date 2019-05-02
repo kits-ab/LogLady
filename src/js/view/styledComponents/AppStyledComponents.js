6cec77e2098298020d1087e2ada26aa009201a2a
jacob.torrang <jacob.torrang@kits.se>
2019-05-02 12:45:29 +0200

    #175 Remove unnecessary CSS

--- src/js/view/styledComponents/AppStyledComponents.js
+++ src/js/view/styledComponents/AppStyledComponents.js
@@ -2,15 +2,13 @@ import styled from 'styled-components';
 
 export const RootContainer = styled.div`
   display: flex;
-  flex-direction: row;
-  background: black;
-  align-items: stretch;
   height: 100vh;
   max-height: 100vh;
   overflow: hidden;
 `;
 
 export const LogPage = styled.div`
+  background: black;
   display: flex;
   flex: 1;
   flex-direction: column;
