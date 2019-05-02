a0e745b15545ecc7cfd1216ac3fe191e9200a75e
jacob.torrang <jacob.torrang@kits.se>
2019-05-02 10:31:53 +0200

    Hotfix away double listeners on startup

--- src/js/view/App.js
+++ src/js/view/App.js
@@ -2,17 +2,11 @@ import TabSettings from './components/TabSettings';
 import LogViewer from './components/LogViewer';
 import TopPanel from './components/TopPanel';
 import DefaultPage from './components/DefaultPage';
-import { createStore } from 'redux';
-import reducers from './reducers/index.js';
-import { ipcListener } from './ipcListener';
 import Statusbar from './components/StatusBar';
 import { RootContainer } from './styledComponents/AppStyledComponents';
 import { connect } from 'react-redux';
 const React = require('react');
 const { Component } = require('react');
-
-const store = createStore(reducers);
-ipcListener(store.dispatch);
 
 class App extends Component {
   render() {
