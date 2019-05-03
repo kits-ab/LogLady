848d88789d6452e46b8c38c82c55c38434abd025
jacob.torrang <jacob.torrang@kits.se>
2019-05-03 13:08:42 +0200

    Disregard faulty regexes and add 学生 as an escape character for normal text

--- src/js/view/reducers/topPanelReducer.js
+++ src/js/view/reducers/topPanelReducer.js
@@ -1,18 +1,3 @@
-function parseRegex(string) {
-  if (!string) return '';
-  if ([...string][0] === '👑') {
-    const regex = [...string].slice(1).join('');
-    try {
-      new RegExp(regex);
-      return regex;
-    } catch (e) {
-      return '';
-    }
-  }
-
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
-
 export const topPanelReducer = (state = { tailSwitch: true }, action) => {
   switch (action.type) {
     case 'tailSwitch':
@@ -23,12 +8,12 @@ export const topPanelReducer = (state = { tailSwitch: true }, action) => {
     case 'filterInput':
       return {
         ...state,
-        filterInput: parseRegex(action.data)
+        filterInput: action.data
       };
     case 'highlightInput':
       return {
         ...state,
-        highlightInput: parseRegex(action.data)
+        highlightInput: action.data
       };
     default:
       return state;
