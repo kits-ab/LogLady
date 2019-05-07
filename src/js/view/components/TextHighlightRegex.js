3fe7d0e778d4a34b7b71b06bb5b7e34475ed5814
jacob.torrang <jacob.torrang@kits.se>
2019-05-07 16:37:12 +0200

    #215 fix edge case when getting '' matches by writing a single ^ or $

--- src/js/view/components/TextHighlightRegex.js
+++ src/js/view/components/TextHighlightRegex.js
@@ -15,14 +15,16 @@ class TextHighlightRegex extends React.Component {
       //if textBeforeMatch is '' it means there was no characters between this match and the previous match, so append it to the previous match
       if (!textBeforeMatch && array.length > 0) {
         array[array.length - 1].highlight += match;
-      } else {
+      } else if (match) {
         //normal text are strings and higlighted items are wrapped in an object so that we can see what text got highlighted later
         array.push(textBeforeMatch);
         array.push({ highlight: match });
+      } else {
+        break; //can't do much if it matches ''
       }
 
       //update input without the text added
-      input = input.slice(Math.max(1, textBeforeMatch.length + match.length));
+      input = input.slice(textBeforeMatch.length + match.length);
     }
 
     //add the text after the last match
@@ -44,7 +46,9 @@ class TextHighlightRegex extends React.Component {
       <TextHighlightRegexSC.HighlightText color={this.props.color}>
         {this.highlightMatches(this.props.text, this.props.regex).map(
           (x, i) => {
-            return x.highlight ? this.toHighlightElement(x.highlight, i) : x;
+            return x.highlight || x.highlight === ''
+              ? this.toHighlightElement(x.highlight, i)
+              : x;
           }
         )}
       </TextHighlightRegexSC.HighlightText>
