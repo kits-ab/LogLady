2c79c685e149d95121cb5ce98eaf6dfca5811731
jacob.torrang <jacob.torrang@kits.se>
2019-05-08 09:09:57 +0200

    #227 Remove unnecessary functions and changed named to convention

--- src/js/view/components/TextHighlightRegex.js
+++ src/js/view/components/TextHighlightRegex.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import reactStringReplace from 'react-string-replace';
-import * as TextHighlightRegexSC from '../styledComponents/TextHighlightRegexStyledComponents';
+import {
+  HighlightMatch,
+  HighlightText
+} from '../styledComponents/TextHighlightRegexStyledComponents';
 
 class TextHighlightRegex extends React.Component {
   highlightMatches = (text, regex) => {
-    //Parenthesis required for reactStringReplace to work properly
     return reactStringReplace(text, regex, (match, i) => {
-      return (
-        <TextHighlightRegexSC.HighlightMatch key={i}>
-          {match}
-        </TextHighlightRegexSC.HighlightMatch>
-      );
+      return <HighlightMatch key={i}>{match}</HighlightMatch>;
     });
-  };
-
-  highlightText = (text, style) => {
-    return <span style={style}>{text}</span>;
   };
 
   render() {
     return (
-      <TextHighlightRegexSC.HighlightText color={this.props.color}>
+      <HighlightText color={this.props.color}>
         {this.highlightMatches(this.props.text, this.props.regex)}
-      </TextHighlightRegexSC.HighlightText>
+      </HighlightText>
     );
   }
 }
