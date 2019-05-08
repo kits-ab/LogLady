fa8791993be0fc2bd5fec8d7238507ced87d2f4b
Yoella <joella.j@hotmail.com>
2019-05-08 08:12:28 +0200

    #240 no longer crash when faulty regular expresion in filter

--- src/js/view/components/helpers/lineFilterHelper.js
+++ src/js/view/components/helpers/lineFilterHelper.js
@@ -1,6 +1,10 @@
 export const findMatches = (lineFilterInput, lineArray) => {
   return lineArray.filter(line => {
-    const regex = new RegExp(lineFilterInput, 'gi');
-    return line.match(regex);
+    try {
+      const regex = new RegExp(lineFilterInput, 'gi');
+      return line.match(regex);
+    } catch (e) {
+      return '';
+    }
   });
 };
