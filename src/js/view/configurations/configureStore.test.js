fdc0c9c462ada9705157e0c6d78e64b68b97a4ad
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-05-17 09:46:05 +0200

    Added mocked functions to interact with the test

--- src/js/view/configurations/configureStore.test.js
+++ src/js/view/configurations/configureStore.test.js
@@ -1,23 +1,24 @@
 import { configureStore } from './configureStore';
 
 it('should saveStateToDisk', () => {
+  const fakeRequester = jest.fn();
   const fakeStore = {
-    getState: () => {
-      console.log('getState invoked');
-      return {
-        logInfoReducer: 'hejhoj',
-        logViewerReducer: 'one',
-        testStuff: 'two'
-      };
-    }
+    getState: jest.fn()
   };
-  const fakeRequester = params => {
-    expect(params).toEqual({
-      function: 'saveState',
-      reduxStateValue: '{"testStuff":"two"}'
-    });
-  };
+
+  fakeStore.getState.mockReturnValue({
+    logInfoReducer: 'hejhoj',
+    logViewerReducer: 'one',
+    testStuff: 'two'
+  });
 
   const publisher = configureStore(fakeStore, fakeRequester);
   publisher.saveStateToDisk();
+  expect(fakeRequester.mock.calls.length).toBe(1);
+  expect(fakeRequester.mock.calls[0]).toEqual([
+    {
+      function: 'saveState',
+      reduxStateValue: '{"testStuff":"two"}'
+    }
+  ]);
 });
