01954b4ec1562c82872aba00a70e828f47041433
jacob.torrang <kopatheonlyone@hotmail.com>
2019-05-20 13:43:43 +0200

    Refactor out styled component for snackbar like we do with other components

--- src/js/view/components/common/snackbar/SnackBar.js
+++ src/js/view/components/common/snackbar/SnackBar.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import styled from 'styled-components';
+import { SnackBarContainer } from 'js/view/styledComponents/common/SnackBarStyledComponents';
 
 export default function SnackBar(props) {
   const [show, setShow] = useState(false);
@@ -20,90 +20,12 @@ export default function SnackBar(props) {
   }, [props.show]);
 
   return (
-    <SnackbarContainer
+    <SnackBarContainer
       fadeOut={props.fadeAfter > 0}
       color={props.color}
       className={show ? 'show' : 'hide'}
     >
       {props.children}
-    </SnackbarContainer>
+    </SnackBarContainer>
   );
 }
-
-const SnackbarContainer = styled.div`
-  position: absolute;
-  min-width: 50%;
-  visibility: hidden;
-  background-color: ${props => {
-    return props.color ? props.color : '#333';
-  }};
-  color: #ccc;
-  bottom: 20px;
-  left: 10px;
-  padding: 10px 16px;
-  border-radius: 4px;
-  z-index: 8;
-
-  &.show {
-    visibility: visible;
-    -webkit-animation: fadein 0.5s;
-    animation: fadein 0.5s;
-  }
-
-  &.hide {
-    ${props => {
-      return props.fadeOut
-        ? `
-    -webkit-animation: fadeout 1.5s;
-    animation: fadeout 1.5s;
-    `
-        : '';
-    }}
-  }
-
-  @-webkit-keyframes fadein {
-    from {
-      bottom: 0;
-      opacity: 0;
-    }
-    to {
-      bottom: 20px;
-      opacity: 1;
-    }
-  }
-
-  @keyframes fadein {
-    from {
-      bottom: 0;
-      opacity: 0;
-    }
-    to {
-      bottom: 20px;
-      opacity: 1;
-    }
-  }
-
-  @-webkit-keyframes fadeout {
-    from {
-      bottom: 20px;
-      opacity: 1;
-      visibility: visible;
-    }
-    to {
-      bottom: 0;
-      opacity: 0;
-    }
-  }
-
-  @keyframes fadeout {
-    from {
-      bottom: 20px;
-      opacity: 1;
-      visibility: visible;
-    }
-    to {
-      bottom: 0;
-      opacity: 0;
-    }
-  }
-`;
---
+++ src/js/view/styledComponents/common/SnackBarStyledComponents.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const SnackbarContainer = styled.div`
+  position: absolute;
+  min-width: 50%;
+  visibility: hidden;
+  background-color: ${props => {
+    return props.color ? props.color : '#333';
+  }};
+  color: #ccc;
+  bottom: 20px;
+  left: 10px;
+  padding: 10px 16px;
+  border-radius: 4px;
+  z-index: 8;
+
+  &.show {
+    visibility: visible;
+    -webkit-animation: fadein 0.5s;
+    animation: fadein 0.5s;
+  }
+
+  &.hide {
+    ${props => {
+      return props.fadeOut
+        ? `
+    -webkit-animation: fadeout 1.5s;
+    animation: fadeout 1.5s;
+    `
+        : '';
+    }}
+  }
+
+  @-webkit-keyframes fadein {
+    from {
+      bottom: 0;
+      opacity: 0;
+    }
+    to {
+      bottom: 20px;
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadein {
+    from {
+      bottom: 0;
+      opacity: 0;
+    }
+    to {
+      bottom: 20px;
+      opacity: 1;
+    }
+  }
+
+  @-webkit-keyframes fadeout {
+    from {
+      bottom: 20px;
+      opacity: 1;
+      visibility: visible;
+    }
+    to {
+      bottom: 0;
+      opacity: 0;
+    }
+  }
+
+  @keyframes fadeout {
+    from {
+      bottom: 20px;
+      opacity: 1;
+      visibility: visible;
+    }
+    to {
+      bottom: 0;
+      opacity: 0;
+    }
+  }
+`;
