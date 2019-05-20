a1ffb9c78c636a5178474b954e81d91636d91a40
jacob.torrang <kopatheonlyone@hotmail.com>
2019-05-20 13:53:47 +0200

    Remove unused import :swimmer:

--- src/js/view/components/common/snackbar/SnackBar.js
+++ src/js/view/components/common/snackbar/SnackBar.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SnackBarContainer } from 'js/view/styledComponents/common/SnackBarStyledComponents';
 
 export default function SnackBar(props) {
