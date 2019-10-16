5e40dae8032afee6b6b656ae5defd618f279cdfd
Victor Davidsson <kontakt@victordavidsson.se>
2019-10-16 17:03:01 +0200

    #258 Update the styling of the LogViewerList elements to work with react-window
    React-window sets the position and dimensions using an inline style, so some of these styling rules were unnecessary.

--- src/js/view/styledComponents/LogViewerListStyledComponents.js
+++ src/js/view/styledComponents/LogViewerListStyledComponents.js
@@ -1,46 +1,26 @@
 import styled from 'styled-components';
-import Color from 'color';
 
 export const LogViewerListContainer = styled.div`
-  min-width: 100%;
+  width: 100%;
   height: 100%;
-  overflow-y: auto;
-  overflow-x: ${props => {
-    return props.wrap ? 'hidden' : 'auto';
-  }};
 `;
 
 export const LogLine = styled.div`
   min-width: 100%;
-  background: ${props => {
-    const color = '#444';
-    return props.index & 1
-      ? color
-      : Color(color)
-          .darken(0.3)
-          .hex();
-  }};
-  ${props => {
-    return props.fixedWidth ? 'width: ' + props.fixedWidth + 'px;' : '';
-  }}
-
-  ${props => {
-    return props.fixedHeight ? 'height: ' + props.fixedHeight + 'px;' : '';
-  }}
-  ${props => {
-    return props.wrap
-      ? `
-    word-wrap: break-word;
-    word-break: break-all;`
-      : 'white-space: nowrap;';
-  }};
+  background-color: #444;
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
+  :nth-child(odd) {
+    background-color: #303030;
+  }
+  white-space: ${props => {
+    return props.wrap ? 'normal' : 'nowrap';
+  }};
 `;
 
 export const LogLineRuler = styled(LogLine)`
   visibility: hidden;
-  min-width: 0px;
-  position: absolute;
+  min-width: unset;
+  position: fixed;
   display: inline-block;
 `;
