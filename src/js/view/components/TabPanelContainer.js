53699af0a02cc152b26ffbe954217c07383681ef
Soroush Nejad <sardabir@gmail.com>
2019-10-29 13:30:18 +0100

    Changed from mobprogramming session

--- src/js/view/components/TabPanelContainer.js
+++ src/js/view/components/TabPanelContainer.js
@@ -1,44 +1,32 @@
 import React, { useState } from 'react';
 import { Tab, TabPanel } from '../styledComponents/TabPanelStyledComponents';
+import { showOpenDialog } from './helpers/handleFileHelper';
 
 export default function TabPanelContainer() {
   const [state, setState] = useState(0);
   const mockLogs = ['loggen', 'loggo', 'loggas'];
+
   function tabOnClick(index) {
     setState(index);
   }
-
-  // const mockLogsF = mockLogs.map((log, index) => {
-  //   return (
-  //     <Tab
-  //       key={index}
-  //       selected={index === state ? true : false}
-  //       index={index}
-  //       onClick={() => {
-  //         tabOnClick(index);
-  //       }}
-  //     >
-  //       {log}
-  //     </Tab>
-  //   );
-  // });
 
   return (
     <TabPanel>
       {mockLogs.map((log, index) => {
         return (
-          <Tab
-            key={index}
-            selected={index === state ? true : false}
-            index={index}
-            onClick={() => {
-              tabOnClick(index);
-            }}
-          >
-            {log}
-          </Tab>
+            <Tab
+              key={index}
+              selected={index === state ? true : false}
+              index={index}
+              onClick={() => {
+                tabOnClick(index);
+              }}
+            >
+              {log}
+            </Tab>
         );
       })}
+      <button onClick={ showOpenDialog } > + </button>
     </TabPanel>
   );
 }
