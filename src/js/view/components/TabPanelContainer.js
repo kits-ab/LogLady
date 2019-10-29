b732660324e5cdb6ebad6b1f10d584dc0afeb317
Johan S <johan@Johans-MacBook-Pro.local>
2019-10-29 13:39:10 +0100

    Connected TabPanelContainer to redux store.

--- src/js/view/components/TabPanelContainer.js
+++ src/js/view/components/TabPanelContainer.js
@@ -1,27 +1,14 @@
 import React, { useState } from 'react';
 import { Tab, TabPanel } from '../styledComponents/TabPanelStyledComponents';
+import { connect } from 'react-redux';
 
-export default function TabPanelContainer() {
+function TabPanelContainer(props) {
   const [state, setState] = useState(0);
   const mockLogs = ['loggen', 'loggo', 'loggas'];
   function tabOnClick(index) {
     setState(index);
   }
-
-  // const mockLogsF = mockLogs.map((log, index) => {
-  //   return (
-  //     <Tab
-  //       key={index}
-  //       selected={index === state ? true : false}
-  //       index={index}
-  //       onClick={() => {
-  //         tabOnClick(index);
-  //       }}
-  //     >
-  //       {log}
-  //     </Tab>
-  //   );
-  // });
+  console.log(props.state);
 
   return (
     <TabPanel>
@@ -42,3 +29,11 @@ export default function TabPanelContainer() {
     </TabPanel>
   );
 }
+
+const mapStateToProps = function(state) {
+  return {
+    state: state
+  };
+};
+
+export default connect(mapStateToProps)(TabPanelContainer);
