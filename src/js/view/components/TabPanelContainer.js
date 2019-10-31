572cf23892433a7a3cb18b52297519b3c6034652
Johan S <johan@Johans-MacBook-Pro.local>
2019-10-31 13:44:02 +0100

    styling and highlighting of tabs.

--- src/js/view/components/TabPanelContainer.js
+++ src/js/view/components/TabPanelContainer.js
@@ -9,15 +9,35 @@ import { showOpenDialog } from './helpers/handleFileHelper';
 import { getFormattedFilePath } from './helpers/StatusBarHelper';
 import { clearLog, updateSourceHandle } from '../actions/dispatchActions';
 function TabPanelContainer(props) {
-  const [state, setState] = useState(0);
 
+  
+  const [state, setState] = useState({});
+  console.log(props.state);
   function tabOnClick(index) {
     updateSourceHandle(props.dispatch, index);
-    setState(index);
+    setState({
+      active: index
+    });
   }
 
-  function exitLog(sourcePath) {
+  function onMouseEnter(index){
+    setState({
+      ...state,
+      hover: index
+    })
+  }
+
+  function onMouseLeave(){
+    setState({
+      ...state,
+      hover: ''
+    })
+  }
+
+  function exitLog(sourcePath, event) {
     clearLog(props.dispatch, sourcePath);
+    event.stopPropagation();
+
   }
 
   return (
@@ -27,21 +47,32 @@ function TabPanelContainer(props) {
           <Tab
             key={props.menuState.openSources[source].index}
             selected={
-              props.menuState.openSources[source].index === state ? true : false
+              props.menuState.openSources[source].index === state.active ? true : false
+            }
+            hover={
+              props.menuState.openSources[source].index === state.hover ? true : false
             }
             index={props.menuState.openSources[source].index}
             onClick={() => {
               tabOnClick(props.menuState.openSources[source].index);
             }}
+            onMouseEnter={()=> {onMouseEnter(props.menuState.openSources[source].index)}}
+            onMouseLeave={onMouseLeave}
           >
             {getFormattedFilePath(
               props.menuState.openSources[source].path,
               `${navigator.platform.startsWith('Win') ? '\\' : '/'}`
             )}
             <Button
-              onClick={() => {
-                exitLog(props.menuState.openSources[source].path);
+              onClick={(event) => {
+                exitLog(props.menuState.openSources[source].path, event);
               }}
+              hover={
+                props.menuState.openSources[source].index === state.hover ? true : false
+              }
+              selected={
+                props.menuState.openSources[source].index === state.active ? true : false
+              }
             >
               X
             </Button>
--- src/js/view/styledComponents/TabPanelStyledComponents.js
+++ src/js/view/styledComponents/TabPanelStyledComponents.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
 export const Tab = styled.div`
+
+  cursor: default;
   background-color: ${props => {
-    return props.selected ? `darkgrey` : `#ccc`;
+    let backgroundColor = '#ccc';
+    if(props.hover && !props.selected) {
+      backgroundColor =  '#B8B8B8'
+    } else if (props.selected) {
+      backgroundColor = 'darkgrey'
+    }
+    return backgroundColor;
   }};
 
   padding-left: 10px;
@@ -16,6 +24,7 @@ export const Tab = styled.div`
     border-left: 1px solid  #222; */
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
+  border-right: 1px solid lightgray;
 `;
 
 export const TabPanel = styled.div`
@@ -23,11 +32,18 @@ export const TabPanel = styled.div`
   flex-direction: row;
   background: #ccc;
   padding-top: 1px;
+  overflow: auto;
+  white-space: nowrap;
+  
 `;
 
 export const Button = styled.div`
-  background: darkgrey;
+  font-size: 15px;
+  opacity: ${props => {
+    return props.hover || props.selected ? '1' : '0';
+  }};
   border-radius: 50%;
   display: inline-block;
   padding: 5px;
+  margin-left: 5px;
 `;
