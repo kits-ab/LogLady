c445f431bb0517614e1c40a3fd1af30305274843
Johan S <johan@Johans-MacBook-Pro.local>
2019-11-12 18:00:53 +0100

    cleaned up some dev stuff.

--- src/js/view/AppContainer.js
+++ src/js/view/AppContainer.js
@@ -8,8 +8,7 @@ import { configureStore } from './configurations/configureStore';
 const React = require('react');
 
 const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducers
 );
 
 const publisher = configureStore(store);
