7bfd05028d3c28c223b72961426c1f66af7e69eb
Doverstav <pontus.doverstav@kits.se>
2019-12-11 19:53:42 +0100

    Add overflow auto to list container so it is possible to scroll in LogList

--- src/js/view/styledComponents/LogViewerListStyledComponents.js
+++ src/js/view/styledComponents/LogViewerListStyledComponents.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const LogViewerListContainer = styled.div`
   width: 100%;
   height: 100%;
+  overflow: auto;
 `;
 
 export const LogLine = styled.div`
