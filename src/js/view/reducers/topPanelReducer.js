6a93dceb5548c3ec3df5e2b6599939924f635895
Doverstav <pontus.doverstav@kits.se>
2020-02-06 10:40:03 +0100

    #224 Modify topPanelReducer to support per file settings

--- src/js/view/reducers/topPanelReducer.js
+++ src/js/view/reducers/topPanelReducer.js
@@ -1,26 +1,76 @@
-const initialState = {
+const initialTabState = {
   tailSwitch: true,
   highlightInput: '',
   filterInput: ''
 };
 
+const initialState = {
+  settings: {}
+};
+
 export const topPanelReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'tailSwitch':
-      return {
+    case 'tailSwitch': {
+      /*return {
         ...state,
         tailSwitch: !state.tailSwitch
-      };
-    case 'filterInput':
+      };*/
+      const { sourcePath } = action.data;
+
       return {
+        settings: {
+          ...state.settings,
+          [sourcePath]: {
+            ...state.settings[sourcePath],
+            tailSwitch: !state.settings[sourcePath].tailSwitch
+          }
+        }
+      };
+    }
+    case 'filterInput': {
+      /*return {
         ...state,
         filterInput: action.data
-      };
-    case 'highlightInput':
+      };*/
+      const { sourcePath } = action.data;
+
       return {
+        settings: {
+          ...state.settings,
+          [sourcePath]: {
+            ...state.settings[sourcePath],
+            filterInput: action.data.text
+          }
+        }
+      };
+    }
+    case 'highlightInput': {
+      /*return {
         ...state,
         highlightInput: action.data
+      };*/
+      const { sourcePath } = action.data;
+
+      return {
+        settings: {
+          ...state.settings,
+          [sourcePath]: {
+            ...state.settings[sourcePath],
+            highlightInput: action.data.text
+          }
+        }
       };
+    }
+    case 'CREATE_SETTINGS_OBJECT': {
+      console.log('creating object');
+      const { sourcePath } = action.data;
+      return {
+        settings: {
+          ...state.settings,
+          [sourcePath]: initialTabState
+        }
+      };
+    }
     case 'TOP_PANEL_STATE_RESTORE':
       return { ...initialState, ...action.data };
     default:
