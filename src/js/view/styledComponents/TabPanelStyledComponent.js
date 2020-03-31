4f6d2a9471412d591581b41fe43c0c686389e8f0
Mohammed Belcaid <belcaidschool@gmail.com>
2020-03-31 17:12:55 +0200

    Updated the position of LogFile Review Activity Spinner

--- src/js/view/styledComponents/TabPanelStyledComponent.js
+++ src/js/view/styledComponents/TabPanelStyledComponent.js
@@ -1,31 +1,10 @@
 import styled from 'styled-components';
 
-export const Indicator = styled.svg`
-  -webkit-animation: action 0.6s infinite alternate;
-  animation: action 0.6s infinite alternate;
-  margin: -10px 0 0 -10px;
-  width: 20px;
-  height: 20x;
+export const Indicator = styled.div`
   display: inline-block;
   position: relative;
-  top: 2px;
-  left: 14px;
-  @-webkit-keyframes action {
-    0% {
-      transform: translateY(0);
-    }
-    100% {
-      transform: translateY(-5px);
-    }
-  }
-  @keyframes action {
-    0% {
-      transform: translateY(0);
-    }
-    100% {
-      transform: translateY(-3px);
-    }
-  }
+  top: -1px;
+  left: 8px;
   opacity: ${props => {
     let opacity = 0;
     if (!props.selected && props.activity) {
