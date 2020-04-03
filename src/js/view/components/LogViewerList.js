dd244d1918ce25f039cc12b7988057d930f6307c
Atmanea <emilia.nilsson@kits.se>
2020-04-03 19:13:29 +0200

    Moved the ref constant up to the other ref hooks.

--- src/js/view/components/LogViewerList.js
+++ src/js/view/components/LogViewerList.js
@@ -26,6 +26,9 @@ const LogViewerList = props => {
   const [maxLineLength, setCurrentMaxLineLength] = useState(1); // Used to save and update how many characters the longest line has
   const [lastLineCount, setLastLineCount] = useState(0); // Used to keep track of how many lines there were last render, for optimizing mainly calculation of new lines
 
+  //listRef is used to get the reference to the List object so that we can use its method forceUpdate
+  const listRef = useRef();
+
   const logViewerListContainerRef = useRef();
   const [listDimensions, setListDimensions] = useState({
     width: 575,
@@ -130,8 +133,6 @@ const LogViewerList = props => {
     );
   };
 
-  //listRef is used to get the reference to the List object so that we can use its method forceUpdate
-  const listRef = useRef();
   //Force updates the List when wrapLines changes value
   useEffect(() => {
     listRef.current.forceUpdate();
