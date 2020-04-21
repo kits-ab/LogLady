1959ef80d2a81fe4fd695b2588a9ebcef975fc6d
Alexander Teklemariam <alexander.teklemariam@gmail.com>
2020-04-21 08:55:36 +0200

    #484 Altered tests to better suit new frontend-cache location

--- src/js/view/components/helpers/logHelper.test.js
+++ src/js/view/components/helpers/logHelper.test.js
@@ -60,52 +60,88 @@ describe('createRegexReducer', () => {
   });
 });
 
-// describe('updateLogViewerCache', () => {
-//   it('should add new lines from index 0', () => {
-//     const totalCacheLength = 8;
-//     const newLines = ['c', 'd', 'a'];
-//     const startIndex = 0;
-//     const expectedResult = ['c', 'd', 'a', '.', '.', '.', '.', '.'];
-//     const result = updateLogViewerCache(totalCacheLength).insertRows(
-//       startIndex,
-//       newLines
-//     );
-//     expect(result).toEqual(expectedResult);
-//   });
+describe('updateLogViewerCache', () => {
+  it('should add new lines from index 0', () => {
+    const totalCacheLength = 8;
+    const newLines = ['c', 'd', 'a'];
+    const startIndex = 0;
+    const expectedResult = [
+      'c',
+      'd',
+      'a',
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ];
+    const result = updateLogViewerCache(totalCacheLength).insertRows(
+      startIndex,
+      newLines
+    );
+    expect(result).toEqual(expectedResult);
+  });
 
-//   it('should add all new lines to the end and stay the same size if startIndex + length of new lines is bigger than size of cache', () => {
-//     const totalCacheLength = 8;
-//     const newLines = ['c', 'd', 'e'];
-//     const startIndex = 7;
-//     const expectedResult = ['.', '.', '.', '.', '.', 'c', 'd', 'e'];
-//     const result = updateLogViewerCache(totalCacheLength).insertRows(
-//       startIndex,
-//       newLines
-//     );
-//     expect(result).toEqual(expectedResult);
-//   });
+  it('should add all new lines to the end and stay the same size if startIndex + length of new lines is bigger than size of cache', () => {
+    const totalCacheLength = 8;
+    const newLines = ['c', 'd', 'e'];
+    const startIndex = 7;
+    const expectedResult = [
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      'c',
+      'd',
+      'e'
+    ];
+    const result = updateLogViewerCache(totalCacheLength).insertRows(
+      startIndex,
+      newLines
+    );
+    expect(result).toEqual(expectedResult);
+  });
 
-//   it('should add lines to index in the middle of the cache', () => {
-//     const totalCacheLength = 8;
-//     const newLines = ['c', 'd', 'e'];
-//     const startIndex = 3;
-//     const expectedResult = ['.', '.', '.', 'c', 'd', 'e', '.', '.'];
-//     const result = updateLogViewerCache(totalCacheLength).insertRows(
-//       startIndex,
-//       newLines
-//     );
-//     expect(result).toEqual(expectedResult);
-//   });
+  it('should add lines to index in the middle of the cache', () => {
+    const totalCacheLength = 8;
+    const newLines = ['c', 'd', 'e'];
+    const startIndex = 3;
+    const expectedResult = [
+      undefined,
+      undefined,
+      undefined,
+      'c',
+      'd',
+      'e',
+      undefined,
+      undefined
+    ];
+    const result = updateLogViewerCache(totalCacheLength).insertRows(
+      startIndex,
+      newLines
+    );
+    expect(result).toEqual(expectedResult);
+  });
 
-//   it('should add lines from index 0 if the startIndex is less than 0', () => {
-//     const totalCacheLength = 8;
-//     const newLines = ['a', 'b', 'c'];
-//     const startIndex = -3;
-//     const expectedResult = ['a', 'b', 'c', '.', '.', '.', '.', '.'];
-//     const result = updateLogViewerCache(totalCacheLength).insertRows(
-//       startIndex,
-//       newLines
-//     );
-//     expect(result).toEqual(expectedResult);
-//   });
-// });
+  it('should add lines from index 0 if the startIndex is less than 0', () => {
+    const totalCacheLength = 8;
+    const newLines = ['a', 'b', 'c'];
+    const startIndex = -3;
+    const expectedResult = [
+      'a',
+      'b',
+      'c',
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    ];
+    const result = updateLogViewerCache(totalCacheLength).insertRows(
+      startIndex,
+      newLines
+    );
+    expect(result).toEqual(expectedResult);
+  });
+});
