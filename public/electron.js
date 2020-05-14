1f4d50653a216196ceb2d012ed4aaa244d197391
Melinda <sandstrommelinda@gmail.com>
2020-05-14 12:21:54 +0200

    #521 State is now saved to disc on opening of a file and on closing of a tab

--- public/electron.js
+++ public/electron.js
@@ -139,7 +139,7 @@ const createWindow = async () => {
   );
 
   mainWindow.on('close', () => {
-    const argObj = { type: 'QUIT' };
+    const argObj = { type: 'SAVE_CURRENT_STATE' };
     mainWindow.webContents.send('backendMessages', argObj);
   });
   loadingWindow.show();
